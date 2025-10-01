b0ab5aa9dd1f0fed5c1c006483b2da53b80af1a1
mmulyana <mulyan.t20@gmail.com>
2025-10-01 22:16:15 +0700

    feat: setup sonner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import '../assets/css/globals.css'
 import Provider from './provider'
+import { Toaster } from '@/shared/components/ui/sonner'
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -29,6 +30,7 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
 				<Provider>{children}</Provider>
+				<Toaster position='top-center' richColors />
 			</body>
 		</html>
 	)
