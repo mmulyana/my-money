052bf31e227aaec224b541e11ee95186741f5847
mmulyana <mulyan.t20@gmail.com>
2025-10-02 00:37:21 +0700

    feat: new page transaction

---
+++ src/app/dashboard/transaction/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Page() {
+	return (
+		<div className='space-y-4 w-full'>
+			<div className='flex justify-between items-center'>
+				<p className='text-[15px] font-medium text-foreground'>Transaction</p>
+			</div>
+		</div>
+	)
+}
