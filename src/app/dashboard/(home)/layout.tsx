5d7310ff0925988968240a3fd77547288f2a7d72
mmulyana <mulyan.t20@gmail.com>
2025-10-07 19:04:54 +0700

    feat: remove unused file and change route

--- src/app/dashboard/(home)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-	return (
-		<div className='flex gap-4 flex-col md:flex-row'>
-			<div className='flex-1'>{children}</div>
-		</div>
-	)
-}
