efbf5422913f8e0d12c5b4c2e2e815401eb3c030
mmulyana <mulyan.t20@gmail.com>
2025-10-08 16:01:00 +0700

    feat: new imgs

--- src/shared/constants/img.ts
+++ src/shared/constants/img.ts
@@ -3,5 +3,8 @@ export const imgs = [
 	'https://res.cloudinary.com/mulyana/image/upload/v1759399021/icon/shopping-cart_boauay.svg',
 	'https://res.cloudinary.com/mulyana/image/upload/v1759399021/icon/shirt_ynveti.svg',
 	'https://res.cloudinary.com/mulyana/image/upload/v1759399021/icon/barbell_cjmrtv.svg',
-	'https://res.cloudinary.com/mulyana/image/upload/v1759400389/icon/tools-kitchen-2_jflnzg.svg'
+	'https://res.cloudinary.com/mulyana/image/upload/v1759400389/icon/tools-kitchen-2_jflnzg.svg',
+	'https://res.cloudinary.com/mulyana/image/upload/v1759804985/icon/credit-card_yom5wh.svg',
+	'https://res.cloudinary.com/mulyana/image/upload/v1759804985/icon/wallet_fcyyg6.svg',
+	'https://res.cloudinary.com/mulyana/image/upload/v1759804985/icon/cash_fp7n4w.svg'
 ]
