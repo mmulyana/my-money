193fce31d121e442ea77b3f481f0a114447eef8c
mmulyana <mulyan.t20@gmail.com>
2025-10-08 16:03:32 +0700

    feat: update category img

--- src/features/category/components/category-img.tsx
+++ src/features/category/components/category-img.tsx
@@ -11,34 +11,28 @@ export default function CategoryImage({
 		return (
 			<div className='relative w-9 h-9'>
 				<div
-					className='absolute w-9 h-9 rounded-full z-[1] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+					className='absolute w-[35px] h-[35px] rounded-full z-[1] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-muted-foreground'
 					style={{ background: color }}
 				></div>
 				<div
-					className='absolute w-8 h-8 rounded-full opacity-[88%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[2]'
+					className='absolute w-9 h-9 rounded-full opacity-[88%] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[2]'
 					style={{ background: 'white' }}
 				></div>
 
 				<img
-					className='w-6 h-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[2]'
+					className='w-5 h-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[2]'
 					src={url}
 				/>
 				<div
-					className='w-7 h-7 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mix-blend-lighten z-[2]'
+					className='w-8 h-8 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mix-blend-lighten z-[2]'
 					style={{ backgroundColor: color }}
-				></div>
-				<div
-					className='w-8 h-8 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-[px] z-[2]'
-					style={{ borderColor: color }}
 				></div>
 			</div>
 		)
 
 	return (
 		<div className='relative w-9 h-9'>
-			<div
-				className='absolute w-[34px] h-[34px] rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-200'
-			></div>
+			<div className='absolute w-[34px] h-[34px] rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-200'></div>
 			<div
 				className='absolute w-[34px] h-[34px] rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mix-blend-difference'
 				style={{ background: color }}
@@ -49,7 +43,7 @@ export default function CategoryImage({
 			/>
 			<div className='w-[34px] h-[34px] rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 mix-blend-difference bg-white'></div>
 			<div
-				className='w-9 h-9 rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-2'
+				className='w-[35px] h-[35px] rounded-full absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-2'
 				style={{
 					borderColor: color,
 				}}
