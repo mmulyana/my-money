448466a5e5f3f7537cb7397d09bfc9a0dd3f66b5
mmulyana <mulyan.t20@gmail.com>
2025-10-08 16:01:24 +0700

    feat: segment bar comp

---
+++ src/shared/components/common/segment-bar.tsx
@@ -0,0 +1,54 @@
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '@/shared/components/ui/tooltip'
+import { cn } from '@/shared/lib/utils'
+
+type Slice = {
+	id: string | number
+	name: string
+	color: string
+	total: number
+}
+
+type Props = {
+	data: Slice[]
+	total?: number
+}
+
+export function SegmentBar({ data, total }: Props) {
+	const totalSum = total ?? data.reduce((acc, item) => acc + item.total, 0)
+
+	return (
+		<TooltipProvider>
+			<div className='flex w-full h-3 rounded bg-muted-foreground/10 relative gap-1'>
+				{data?.map((item) => {
+					const widthPercent = (item.total / totalSum) * 100
+
+					return (
+						<Tooltip key={item.id}>
+							<TooltipTrigger asChild>
+								<div
+									className={cn(
+										'h-full rounded absolute top-0 left-0 transition-all ease-in'
+									)}
+									style={{
+										width: `${widthPercent}%`,
+										backgroundColor: item.color,
+										position: 'relative',
+									}}
+								/>
+							</TooltipTrigger>
+							<TooltipContent>
+								<p className='text-sm font-medium'>{item.name}</p>
+								<p className='text-xs text-muted-foreground'>{item.total}</p>
+							</TooltipContent>
+						</Tooltip>
+					)
+				})}
+			</div>
+		</TooltipProvider>
+	)
+}
