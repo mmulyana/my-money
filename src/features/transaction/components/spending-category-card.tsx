ba7f52afbec5f9220c5cd4ac7e93e95fa8ddbd37
mmulyana <mulyan.t20@gmail.com>
2025-10-08 18:42:44 +0700

    feat: integrate spending category card

--- src/features/transaction/components/spending-category-card.tsx
+++ src/features/transaction/components/spending-category-card.tsx
@@ -1,8 +1,27 @@
+'use client'
+
+import { useMemo, useState } from 'react'
+
 import CategoryImage from '@/features/category/components/category-img'
+
 import { SegmentBar } from '@/shared/components/common/segment-bar'
 import { imgs } from '@/shared/constants/img'
 
+import { useGetExpenseCategory } from '../api/get-expense-category'
+
 export default function SpendingCategoryCard() {
+	const [currDate] = useState<Date>(() => new Date())
+
+	const { data } = useGetExpenseCategory({
+		date: currDate.toString(),
+		enabled: true,
+	})
+
+	const total = useMemo(() => {
+		if (!data) return 0
+		return data?.data?.reduce((acc, trx) => (acc += Number(trx.total)), 0)
+	}, [JSON.stringify(data?.data)])
+
 	return (
 		<div className='bg-white rounded-lg'>
 			<div className='border-b border-dashed p-4 pb-3 flex justify-between'>
@@ -14,30 +33,40 @@ export default function SpendingCategoryCard() {
 			<div className='p-4 pt-3'>
 				<p className='text-xs text-foreground/50 mb-3'>Wallets</p>
 				<div className='space-y-4'>
-					<div className='flex gap-2 items-start'>
-						<div className='w-9 shrink-0'>
-							<CategoryImage color='#2B88F3' url={imgs[2]} variant='style-1' />
-						</div>
-						<div className='space-y-1 flex-1'>
-							<div className='flex justify-between items-center'>
-								<p className='text-sm text-foreground/80'>Clothes</p>
-								<p className='text-sm text-foreground/80'>80%</p>
+					{data?.data?.map((i) => {
+						const percent = (i.total / total) * 100
+						return (
+							<div className='flex gap-2 items-start' key={i.id}>
+								<div className='w-9 shrink-0'>
+									<CategoryImage
+										color={i.color}
+										url={i.imageUrl}
+										variant={i.imageVariant as any}
+									/>
+								</div>
+								<div className='space-y-1 flex-1'>
+									<div className='flex justify-between items-center'>
+										<p className='text-sm text-foreground/80'>{i.name}</p>
+										<p className='text-sm text-foreground/80'>
+											{percent.toFixed(0)}%
+										</p>
+									</div>
+									<SegmentBar
+										className='bg-transparent'
+										total={total}
+										data={[
+											{
+												id: i.id,
+												name: i.name,
+												color: i.color,
+												total: i.total,
+											},
+										]}
+									/>
+								</div>
 							</div>
-							<SegmentBar
-								total={100}
-								items={[
-									{
-										color: '#2B88F3',
-										value: 80,
-									},
-									{
-										color: '#ECECEC',
-										value: 20,
-									},
-								]}
-							/>
-						</div>
-					</div>
+						)
+					})}
 				</div>
 			</div>
 		</div>
