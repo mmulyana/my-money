cd49708906c636590555133ab2bac18bf7132060
mmulyana <mulyan.t20@gmail.com>
2025-10-22 20:26:13 +0700

    feat: implement budget overview

--- src/features/budget/components/budget-overview-card.tsx
+++ src/features/budget/components/budget-overview-card.tsx
@@ -1,41 +1,40 @@
 import ProgressBar from "@/shared/components/common/progress-bar";
+import { useGetBudget } from "../api/get-budget";
+import useCalendarMonth from "@/shared/hooks/use-calendar-month";
 
 export default function BudgetOverviewCard() {
+  const { month, year } = useCalendarMonth();
+
+  const { data } = useGetBudget({
+    month: +month,
+    year,
+  });
+
   return (
     <div className="p-4 lg:rounded-lg w-full bg-white">
       <p className="text-[13px] font-medium text-foreground mb-4">Budgets</p>
 
       <div className="space-y-4">
-        <div className="pb-2.5 border-b border-muted-foreground/10">
-          <p className="text-[15px] text-foreground/80 mb-1">Maintain</p>
-          <p className="text-base font-medium text-foreground mb-1">
-            4.500.000 IDR
-          </p>
-
-          <ProgressBar progress={20} color="#10A986" />
-          <div className="flex justify-between items-center">
-            <p className="text-sm text-foreground">
-              <span className="opacity-50">Remaining </span>
-              2.100.000 IDR
+        {data?.data?.map((i) => (
+          <div
+            key={i.id}
+            className="pb-2.5 border-b border-muted-foreground/10"
+          >
+            <p className="text-[15px] text-foreground/80 mb-1">{i.name}</p>
+            <p className="text-base font-medium text-foreground mb-1">
+              {i.spent}
             </p>
-            <p></p>
-          </div>
-        </div>
-        <div>
-          <p className="text-[15px] text-foreground/80 mb-1">Maintain</p>
-          <p className="text-base font-medium text-foreground mb-1">
-            4.500.000 IDR
-          </p>
 
-          <ProgressBar progress={20} color="#10A986" />
-          <div className="flex justify-between items-center">
-            <p className="text-sm text-foreground">
-              <span className="opacity-50">Remaining </span>
-              2.100.000 IDR
-            </p>
-            <p></p>
+            <ProgressBar progress={i.usage} color="#10A986" />
+            <div className="flex justify-between items-center">
+              <p className="text-sm text-foreground">
+                <span className="opacity-50">Remaining </span>
+                {i.remaining} IDR
+              </p>
+              <p></p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
