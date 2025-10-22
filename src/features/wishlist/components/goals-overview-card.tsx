f145e3311a08b3e1d58a81eda662bb36e600c1e1
mmulyana <mulyan.t20@gmail.com>
2025-10-22 20:25:49 +0700

    feat: integrate goal overview

--- src/features/wishlist/components/goals-overview-card.tsx
+++ src/features/wishlist/components/goals-overview-card.tsx
@@ -1,57 +1,38 @@
 import ProgressBar from "@/shared/components/common/progress-bar";
-import { Button } from "@/shared/components/ui/button";
+import { useGetWishlist } from "../api/get-wishlist";
 
 export default function GoalsOverviewCard() {
+  const { data } = useGetWishlist({});
   return (
     <div className="p-4 lg:rounded-lg w-full bg-white">
       <p className="text-[13px] font-medium text-foreground mb-4">Goals</p>
 
       <div className="flex flex-col gap-4">
-        <div className="pb-2.5 border-b border-muted-foreground/10">
-          <div className="flex gap-2 items-center mb-1">
-            <div className="h-14 w-14 rounded-md bg-gray-300"></div>
-            <div className="flex flex-col justify-center items-start">
-              <p className="text-[15px] text-foreground/80">iPhone 13</p>
-              <p className="text-base font-medium text-foreground">
-                4.500.000 IDR
+        {data?.data?.map((i) => (
+          <div
+            key={i.id}
+            className="pb-2.5 border-b border-muted-foreground/10"
+          >
+            <div className="flex gap-2 items-center mb-1">
+              <div className="h-14 w-14 rounded-md bg-gray-300"></div>
+              <div className="flex flex-col justify-center items-start">
+                <p className="text-[15px] text-foreground/80">{i.name}</p>
+                <p className="text-base font-medium text-foreground">
+                  {i.total}
+                </p>
+              </div>
+            </div>
+
+            <ProgressBar progress={i.progress} color="#10A986" />
+            <div className="flex justify-between items-center">
+              <p className="text-sm text-foreground">
+                <span className="opacity-50">Allocate </span>
+                200 IDR/Day <span className="opacity-50">to stay on track</span>
               </p>
+              <p></p>
             </div>
           </div>
-
-          <ProgressBar progress={20} color="#10A986" />
-          <div className="flex justify-between items-center">
-            <p className="text-sm text-foreground">
-              <span className="opacity-50">Allocate </span>
-              200 IDR/Day <span className="opacity-50">to stay on track</span>
-            </p>
-            <p></p>
-          </div>
-        </div>
-        <div>
-          <div className="flex gap-2 items-center mb-1">
-            <div className="h-14 w-14 rounded-md bg-gray-300"></div>
-            <div className="flex flex-col justify-center items-start">
-              <p className="text-[15px] text-foreground/80">Macbook Pro</p>
-              <p className="text-base font-medium text-foreground">
-                4.500.000 IDR
-              </p>
-            </div>
-          </div>
-
-          <ProgressBar progress={20} color="#10A986" />
-          <div className="flex justify-between items-center">
-            <p className="text-sm text-foreground">
-              <span className="opacity-50">your off track </span>
-              <Button
-                variant={"link"}
-                className="h-fit p-0 text-foreground font-normal"
-              >
-                adjust goals
-              </Button>
-            </p>
-            <p></p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
